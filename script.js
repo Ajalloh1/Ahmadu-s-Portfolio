64a4a45fc01f49d08cfd9c4ea2632825636f2fa5
Ajalloh1 <mys4jalloh@gmail.com>
2022-07-23 22:47:01 -0400

    added portfolio

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-var img1 = document.getElementById("workim1");
-var img4 = document.getElementById("workim2");
+// var img1 = document.getElementById("workim1");
+// var img4 = document.getElementById("workim2");
 
 
-img1.addEventListener("click", function () {
-    console.log("here")
-    var imageBox = document.createElement("div");
-    var image = document.createElement("img");
-    image.setAttribute("src", "image3.jpeg");
-    image.setAttribute("class", "img1")
-    imageBox.setAttribute("class", "image1");
-    document.body.appendChild(imageBox);
-    imageBox.appendChild(image);
-})
+// img1.addEventListener("click", function () {
+//     console.log("here")
+//     var imageBox = document.createElement("div");
+//     var image = document.createElement("img");
+//     image.setAttribute("src", "image3.jpeg");
+//     image.setAttribute("class", "img1")
+//     imageBox.setAttribute("class", "image1");
+//     document.body.appendChild(imageBox);
+//     imageBox.appendChild(image);
+// })
 
-img4.addEventListener("click", function () {
-    console.log("here")
-    var imageBox = document.createElement("div");
-    var image = document.createElement("img");
-    image.setAttribute("src", "image3.jpeg");
-    image.setAttribute("class", "img4")
-    imageBox.setAttribute("class", "image1");
-    document.body.appendChild(imageBox);
-    imageBox.appendChild(image);
-})
+// img4.addEventListener("click", function () {
+//     console.log("here")
+//     var imageBox = document.createElement("div");
+//     var image = document.createElement("img");
+//     image.setAttribute("src", "image3.jpeg");
+//     image.setAttribute("class", "img4")
+//     imageBox.setAttribute("class", "image1");
+//     document.body.appendChild(imageBox);
+//     imageBox.appendChild(image);
+// // })
+// const ul = (document.querySelectorAll("p"));
+
